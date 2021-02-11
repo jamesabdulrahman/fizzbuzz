6074e1193248310924af14a5597e71834ad3eb3f
James Brierley <james.brierley@ibm.com>
2021-02-11 14:34:45 +0000

    add class for main

---
+++ FizzBuzzMain.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class FizzBuzzMain {
+
+  public static void main(String[] args) {
+    FizzBuzz fb = new FizzBuzz();
+    Random random = new Random();
+    int fizzbuzzwoofs = 0;
+    for (int i = 0; i <= 1000; i++) {
+      int rn = random.nextInt();
+      if (fb.fizzbuzz(rn).equals("fizzbuzzwoof")) {
+        fizzbuzzwoofs++;
+      }
+    }
+    System.out.println(fizzbuzzwoofs);
+  }
+}
